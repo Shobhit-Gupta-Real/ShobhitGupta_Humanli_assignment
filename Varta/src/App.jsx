a87687f8d9aec9a784d3eb3f4433c4533d7c7481
Shobhit Gupta <guptashobhit2024@gmail.com>
2024-03-03 07:56:08 +0530

    route-protection

--- Varta/src/App.jsx
+++ Varta/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import MainContainer from './Components/MainContainer'
 import Login from './Components/Login'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Welcome from './Components/Welcome'
 import ChatArea from './Components/ChatArea'
 import User_Groups from './Components/User_Groups'
@@ -14,12 +14,21 @@ import { useAuth } from './Context/AuthContext'
 function App() {
   const [count, setCount] = useState(0)
   const {currentUser} = useAuth()
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/" />;
+    }
 
+    return children
+  };
   return (
     <div className='app'>
       <Routes>
         <Route path='/' element={<Login/>}/>
-        <Route path='varta' element={<MainContainer/>}>
+        <Route path='varta' element={
+        <ProtectedRoute>
+        <MainContainer/>
+        </ProtectedRoute>}>
           <Route path='welcome' element={<Welcome/>}/>
           <Route path='chat' element={<ChatArea/>}/>
           <Route path='users' element={<User_Groups/>}/>
